54478f5b9126d1f6da0a80a51e3170a41fb583fc
unknown <dougrchamberlain@gmail.com>
2018-02-16 12:52:27 -0500

    most basic setup complete and builds

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: { app: './src/index.ts' },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hacker App'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
